e52bcef1d681d5b7d9c2e68ae691215497c6dc00
shuntaro <shun.sea.f@gmail.com>
2020-08-30 22:36:05 +0900

    新規登録画面更新

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,56 +4,58 @@
 @include("header")
 <div class="register-container">
 
-        <form method="POST" class="register-form" action="{{ route('register') }}" enctype="multipart/form-data">
-            @csrf
+    <div class="background-image"></div>
+    
+    <form method="POST" class="register-form" action="{{ route('register') }}" enctype="multipart/form-data">
+        @csrf
+        
+        <div>
+            <input id="name" class="input-group" type="text" name="name" value="{{ old('name') }}" required autocomplete="name" placeholder="Name..." autofocus>
+
+            <div>
+                @error('name')
+                    <span class="invalid-feedback" role="alert">
+                        <p class="form-error">{{ $message }}</p>
+                    </span>
+                @enderror
+            </div>
+        </div>
+
+        <div>
+            <input id="email" class="input-group" type="email" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Email...">
             
             <div>
-                <input id="name" class="input-group" type="text" name="name" value="{{ old('name') }}" required autocomplete="name" placeholder="Name..." autofocus>
-
-                <div>
-                    @error('name')
-                        <span class="invalid-feedback" role="alert">
-                            <p class="form-error">{{ $message }}</p>
-                        </span>
-                    @enderror
-                </div>
+                @error('email')
+                    <span role="alert">
+                        <p class="form-error">{{ $message }}</p>
+                    </span>
+                @enderror
             </div>
+        </div>
+
+        <div>
+            <input id="password" class="input-group" type="password" name="password"  required autocomplete="current-password" placeholder="Password...">
 
             <div>
-                <input id="email" class="input-group" type="email" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Email...">
-                
-                <div>
-                    @error('email')
-                        <span role="alert">
-                            <p class="form-error">{{ $message }}</p>
-                        </span>
-                    @enderror
-                </div>
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <p class="form-error">{{ $message }}</p>
+                    </span>
+                @enderror
             </div>
+        </div>
 
-            <div>
-                <input id="password" class="input-group" type="password" name="password"  required autocomplete="current-password" placeholder="Password...">
+        <div>
+            <input id="password-confirm" class="input-group" type="password" name="password_confirmation" required autocomplete="new-password" placeholder="Password Confirmation...">
 
-                <div>
-                    @error('password')
-                        <span class="invalid-feedback" role="alert">
-                            <p class="form-error">{{ $message }}</p>
-                        </span>
-                    @enderror
-                </div>
-            </div>
+        </div>
 
-            <div>
-                <input id="password-confirm" class="input-group" type="password" name="password_confirmation" required autocomplete="new-password" placeholder="Password Confirmation...">
-
-            </div>
-
-            <button type="submit" class="btn btn-primary">
-                Sign up
-            </button>
+        <button type="submit" class="btn btn-primary">
+            Sign up
+        </button>
 
 
-        </form>
-    </div>
+    </form>
+    
 </div>
 @endsection
