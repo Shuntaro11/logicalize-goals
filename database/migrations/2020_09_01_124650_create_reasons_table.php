93bc475d4bcbd85b386aa0bbf4f63ce20f1d660c
shuntaro <shun.sea.f@gmail.com>
2020-09-01 21:56:12 +0900

    reasonsテーブル作成

---
+++ database/migrations/2020_09_01_124650_create_reasons_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReasonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reasons', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('reason');
+            $table->integer('achievement')->default(0);
+            $table->bigInteger('goal_id')->unsigned()->index();
+            $table->timestamps();
+            $table->foreign('goal_id')->references('id')->on('goals')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reasons');
+    }
+}
