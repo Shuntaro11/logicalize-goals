64511179ea38ed2042de79eb69440258a3fda803
shuntaro <shun.sea.f@gmail.com>
2020-09-04 00:11:00 +0900

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
     </title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ mix('js/app.js') }}" defer></script>
     @yield('script')
 
     <!-- Styles -->
