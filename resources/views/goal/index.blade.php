f0a72a3ed2418576f4e7a085a70164fb08adc402
shuntaro <shun.sea.f@gmail.com>
2020-09-04 00:34:45 +0900

    Update index.blade.php

--- resources/views/goal/index.blade.php
+++ resources/views/goal/index.blade.php
@@ -5,7 +5,7 @@
 @section('script')
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-    <script src="{{ asset('/js/pull-down.js') }}" defer></script>
+    <script src="{{ mix('/js/pull-down.js') }}" defer></script>
     
 @endsection
 
