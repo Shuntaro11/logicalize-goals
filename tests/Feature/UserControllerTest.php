5d8e2d61c5f789d628369755405ca78726390897
shuntaro <shun.sea.f@gmail.com>
2020-09-20 23:13:27 +0900

    UserControllerテスト設定

--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -2,21 +2,75 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Support\Facades\Auth;
 
 class UserControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
-     * A basic feature test example.
+     * A basic test example.
      *
+     * @test
      * @return void
      */
-    public function testExample()
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
+    public function test_ユーザー新規登録()
+    {
+        
+        $this->post(route('register'), [
+            'name' => 'user',
+            'email' => 'user@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ])
+            ->assertStatus(302);
+        $this->assertDatabaseHas('users', ['email' => 'user@example.com']);
+
     }
+
+    public function test_ユーザーログイン()
+    {
+        
+        $user = factory(User::class)->create();
+
+        // 認証されていることを確認
+        $this->assertTrue(Auth::check());
+
+        $response = $this
+            ->actingAs($user)
+            ->get('/goals/index');
+
+        // goals.indexに遷移すると「目標一覧」が表示される
+        $response->assertStatus(200)
+            ->assertViewIs('goal.index')
+            ->assertSee('目標一覧');
+
+    }
+
+
+    public function test_ユーザーログアウト()
+    {
+        $user = factory(User::class)->create();
+    
+        $this->actingAs($user);
+    
+        // 認証されていることを確認
+        $this->assertTrue(Auth::check());
+    
+        // ログアウトを実行
+        $response = $this->post('logout');
+    
+        // 認証されていない
+        $this->assertFalse(Auth::check());
+    
+         // topページにリダイレクトする
+        $response->assertRedirect('/');
+
+    }
+
 }
