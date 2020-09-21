4a5e0fd4dcb9920f31cc9bdfa65c50df856641dc
shuntaro <shun.sea.f@gmail.com>
2020-09-21 23:44:55 +0900

    Update GoalControllerTest.php

--- tests/Feature/GoalControllerTest.php
+++ tests/Feature/GoalControllerTest.php
@@ -32,7 +32,98 @@ class GoalControllerTest extends TestCase
             'why1' => 'テスト理由１',
             'step1' => 'テストステップ１',
         ])
-            ->assertStatus(302);
+            ->assertStatus(200);
         $this->assertDatabaseHas('goals', ['what' => 'テスト目標１']);
+    }
+
+
+    public function test_「why」と「step」が無くてもgoalを登録できる()
+    {
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+        $this->assertTrue(Auth::check());
+
+        $this->post(route('goals.store'), [
+            'what' => 'テスト目標２',
+            'when' => '2030-01-01',
+            'how_important' => '1',
+            'how_urgent' => '1',
+        ])
+            ->assertStatus(200);
+        $this->assertDatabaseHas('goals', ['what' => 'テスト目標２']);
+    }
+
+
+    public function test_「how_important」が10より大きいとgoalを登録できない()
+    {
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+        $this->assertTrue(Auth::check());
+
+        $this->post(route('goals.store'), [
+            'what' => 'テスト目標３',
+            'when' => '2030-01-01',
+            'how_important' => '11',
+            'how_urgent' => '1',
+            'why1' => 'テスト理由３',
+            'step1' => 'テストステップ３',
+        ])
+            ->assertStatus(302);
+        $this->assertDatabaseMissing('goals', ['what' => 'テスト目標３']);
+    }
+
+
+    public function test_「how_urgent」が10より大きいとgoalを登録できない()
+    {
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+        $this->assertTrue(Auth::check());
+
+        $this->post(route('goals.store'), [
+            'what' => 'テスト目標４',
+            'when' => '2030-01-01',
+            'how_important' => '1',
+            'how_urgent' => '11',
+            'why1' => 'テスト理由４',
+            'step1' => 'テストステップ４',
+        ])
+            ->assertStatus(302);
+        $this->assertDatabaseMissing('goals', ['what' => 'テスト目標４']);
+    }
+
+
+    public function test_「what」が無いとgoalを登録できない()
+    {
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+        $this->assertTrue(Auth::check());
+
+        $this->post(route('goals.store'), [
+            'when' => '2030-01-01',
+            'how_important' => '1',
+            'how_urgent' => '1',
+            'why1' => 'テスト理由５',
+            'step1' => 'テストステップ５',
+        ])
+            ->assertStatus(302);
+        $this->assertDatabaseMissing('goals', ['what' => 'テスト目標５']);
+    }
+
+
+    public function test_「when」が無いとgoalを登録できない()
+    {
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+        $this->assertTrue(Auth::check());
+
+        $this->post(route('goals.store'), [
+            'what' => 'テスト目標６',
+            'how_important' => '1',
+            'how_urgent' => '1',
+            'why1' => 'テスト理由６',
+            'step1' => 'テストステップ６',
+        ])
+            ->assertStatus(302);
+        $this->assertDatabaseMissing('goals', ['what' => 'テスト目標６']);
     }
 }
